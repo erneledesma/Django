0e72c34149cee7992983ec67608f527505b48dd0
Tito Ledesma <ernestoledesma1@MacBook-Air-de-Tito.local>
2015-05-28 22:10:09 -0300

    models.py
    se agrego un uploads de tracks

--- sfotipy/tracks/models.py
+++ sfotipy/tracks/models.py
@@ -5,3 +5,4 @@ class Tracks(models.Model):
 	tittle = models.CharField(max_lenght=255)
 	order  = models.PositiveIntegerField()
 	track_file = models.FileField(upload_to='tracks')
+	track_file = models.FileField(upload_to='tracks')
